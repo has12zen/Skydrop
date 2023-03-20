db9c50e0c1d19ebf078ebc83a72124b193523d79
has12zen <has12@iitbbs.ac.in>
2023-03-20 11:25:40 +0530

    JWT accessToken

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const {promisify} = require("util");
-const jwt = require('jsonwebtoken');
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
@@ -9,6 +9,7 @@ const AppError = require("../utils/appError");
 const { getAuth } = require("firebase-admin/auth");
 
 const signToken = (token) => {
+  console.log({token})
   return jwt.sign({ token }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
@@ -16,9 +17,16 @@ const signToken = (token) => {
 
 exports.verifyToken = catchAsync(async (req, res, next) => {
   let { accessToken } = req.body;
-  if(!accessToken) accessToken = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+  if (!accessToken) {
+    accessToken = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+    accessToken=accessToken.token;
+  }
 
-  console.log({accessToken});
+  console.log({ accessToken });
+  req.accessToken=accessToken;
 
   if (!accessToken)
     return next(
@@ -55,7 +63,6 @@ exports.restrictTo = (...roles) => {
 exports.login = catchAsync(async (req, res, next) => {
   let user = await User.getUserByEmail(req.user.email);
 
-
   if (!user) {
     await User.create({
       email: req.user.email,
@@ -71,14 +78,14 @@ exports.login = catchAsync(async (req, res, next) => {
       id: req.user.uid,
     };
   }
-  const token = signToken(user._id);
+  const token = signToken(req.accessToken);
 
-  res.cookie('jwt', token, {
+  res.cookie("jwt", token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.header('x-forwarded-proto') === 'https',
+    secure: req.secure || req.header("x-forwarded-proto") === "https",
   });
   res.send(user);
 });

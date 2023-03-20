fd28995ceae3a57ab39dfbc3914ca47a961f52a1
has12zen <has12@iitbbs.ac.in>
2023-03-20 10:56:18 +0530

    import jwt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
+const jwt = require('jsonwebtoken');
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
